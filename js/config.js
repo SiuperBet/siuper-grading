b1ee046c0a21dcb92d7bdcba891596c9296ddee4
SiuperBet <cristian.betelli97@gmail.com>
2026-09-26 15:28:06 +0200

    Bump app version to 0.1.7

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const APP_VERSION="0.1.6";
+export const APP_VERSION="0.1.7";
 export const DATABASE_VERSION=1;
 export const GRADING_ALGORITHM_VERSION="0.2.0";
 export const PRICE_ENGINE_VERSION="0.1.0";
