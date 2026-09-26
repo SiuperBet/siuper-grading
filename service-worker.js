54ca430e3123f18dd8bf63c0c7cacbb15978b305
SiuperBet <cristian.betelli97@gmail.com>
2026-09-26 15:38:44 +0200

    Refresh PWA cache for reliable auto updates

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE="siuper-grading-shell-v9";
-const SHELL=["./","./index.html","./css/app.css","./manifest.webmanifest","./assets/icon.svg","./js/config.js","./js/normalize.js","./js/db.js","./js/catalog.js","./js/collection.js","./js/scanner.js","./js/recognition.js","./js/grading.js","./js/prices.js","./js/mastersets.js","./js/price-history.js","./js/data/pokemon-adapter.js","./js/data/yugioh-adapter.js","./js/app.js"];
+const CACHE="siuper-grading-shell-v10";
+const SHELL=["./","./index.html","./css/app.css","./css/app.css?v=0.1.10","./manifest.webmanifest","./assets/icon.svg","./js/config.js","./js/normalize.js","./js/db.js","./js/catalog.js","./js/collection.js","./js/scanner.js","./js/recognition.js","./js/grading.js","./js/prices.js","./js/mastersets.js","./js/price-history.js","./js/data/pokemon-adapter.js","./js/data/yugioh-adapter.js","./js/app.js","./js/app.js?v=0.1.10"];
 
 self.addEventListener("install",e=>{
   e.waitUntil(caches.open(CACHE).then(c=>Promise.all(SHELL.map(x=>c.add(x).catch(()=>null)))));
