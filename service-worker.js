47bbfd4569e21764b64b3c036e66b151b7b8d906
SiuperBet <cristian.betelli97@gmail.com>
2026-09-26 13:07:49 +0200

    Refresh PWA cache for master-set cards

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE="siuper-grading-shell-v2";
+const CACHE="siuper-grading-shell-v3";
 const SHELL=["./","./index.html","./css/app.css","./manifest.webmanifest","./assets/icon.svg","./js/config.js","./js/normalize.js","./js/db.js","./js/catalog.js","./js/collection.js","./js/scanner.js","./js/recognition.js","./js/grading.js","./js/prices.js","./js/mastersets.js","./js/price-history.js","./js/data/pokemon-adapter.js","./js/data/yugioh-adapter.js","./js/app.js"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>Promise.all(SHELL.map(x=>c.add(x).catch(()=>null)))).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
